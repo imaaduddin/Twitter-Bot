1161a834549157a29eaeb0b0956d3082b85999cf
Imaad Uddin <imaaduddin@icloud.com>
2021-03-02 11:57:36 -0600

    added Twitter API keys

--- tweet.py
+++ tweet.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler("W5QG0YHF33udZbgkXUWKcR5dT", "wIJteRKUVGlAmvXpN60go0ym4GCcdpx0M4AmoYMNibdtN6mrg5")
+auth.set_access_token("1292444629-zscLtnFoEY0d8tvJCG0MzT6JmbWrSCESP5ViH5X", "Myqi0l4jrrpzDrsfMKhs9KPEmClQXTBoI975UjkyOBqgj")
 
 api = tweepy.API(auth)
 
@@ -9,4 +9,3 @@ public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
 
-    
