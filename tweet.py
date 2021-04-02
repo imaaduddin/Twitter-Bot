d87709f7f91f8400ae6e9d1cd53de7fc800c8e04
Imaad Uddin <imaaduddin@icloud.com>
2021-04-01 21:33:30 -0500

    follow back

--- tweet.py
+++ tweet.py
@@ -1,5 +1,5 @@
 import tweepy
-# import time
+import time
 
 auth = tweepy.OAuthHandler("W5QG0YHF33udZbgkXUWKcR5dT", "wIJteRKUVGlAmvXpN60go0ym4GCcdpx0M4AmoYMNibdtN6mrg5")
 auth.set_access_token("1292444629-zscLtnFoEY0d8tvJCG0MzT6JmbWrSCESP5ViH5X", "Myqi0l4jrrpzDrsfMKhs9KPEmClQXTBoI975UjkyOBqgj")
@@ -12,18 +12,19 @@ user = api.me()
 # for tweet in public_tweets:
 #     print(tweet.text)
 
-# def limit_handle(cursor):
-#   try:
-#     while True:
-#       yield cursor.next()
-#   except tweepy.RateLimitError:
-#     time.sleep(1000)
+def limit_handle(cursor):
+  try:
+    while True:
+      yield cursor.next()
+  except tweepy.RateLimitError:
+    time.sleep(1000)
 
 
 # Genrous Bot (follows back all your followers)
-# for follower in limit_handler(tweepy.Cursor(api.followers).items()):
-#   print(follower.name)
-
-# Generous Bot 
-for follower in tweepy.Cursor(api.followers).items():
+for follower in limit_handle(tweepy.Cursor(api.followers).items()):
+  # to follow someone
+  # if follower.name == "follower name":
+  #   follower.follow()
   print(follower.name)
+
+
